22dbf0ed2de65ef84c885bdaaff7bfaefdf99c9b
UserHome <Madiyar5404>
2023-03-06 13:36:50 +0600

    Initial commit

---
+++ app/src/main/java/com/example/bookappdemo/PdfListAdminActivity.java
@@ -0,0 +1,15 @@
+package com.example.bookappdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PdfListAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_list_admin);
+
+    }
+}
